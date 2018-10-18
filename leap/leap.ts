80720b1bb90e688817ef3acbbabad5e9ada0f409
Peter Dimou <git@damoo.zone>
2018-10-18 15:41:45 -0400

    Implement alternative solution using pure Boolean logic for leap

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,11 +1,14 @@
 function isLeapYear(year: number): boolean {
-  if (year % 4 === 0) {
+  /*
+    if (year % 4 === 0) {
     if (year % 100 === 0) {
       return year % 400 === 0;
     }
     return true;
   }
   return false;
+  */
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 }
 
 export default isLeapYear;
