ad2c6e7cac3363dd5b7a2fb74aaf52e66f4ab303
Peter Dimou <git@damoo.zone>
2018-10-30 15:22:25 -0400

    Implement strain via reduce

--- strain/strain.ts
+++ strain/strain.ts
@@ -1,7 +1,17 @@
 export function keep<T>(data: T[], callback: (n: T) => boolean): T[] {
-  return data;
+  return data.reduce((prev, curr) => {
+    if (callback(curr)) {
+      prev.push(curr);
+    }
+    return prev;
+  }, new Array<T>());
 }
 
 export function discard<T>(data: T[], callback: (n: T) => boolean): T[] {
-  return data;
+  return data.reduce((prev, curr) => {
+    if (!callback(curr)) {
+      prev.push(curr);
+    }
+    return prev;
+  }, new Array<T>());
 }
