d9d8051ea4a999d97d7f509ddd2011be38c71155
Peter Dimou <git@damoo.zone>
2018-11-13 10:31:24 -0500

    Refactor pangram regex name

--- pangram/pangram.ts
+++ pangram/pangram.ts
@@ -1,12 +1,15 @@
 export default class Pangram {
   private readonly text: string;
-  private static readonly REGEX = /([a-z])(?!.*\1)/g;
+  private static readonly ALPHA_UNIQ_COUNT_PATTERN = /([a-z])(?!.*\1)/g;
 
   constructor(text: string) {
     this.text = text;
   }
 
   public isPangram(): boolean {
-    return (this.text.toLowerCase().match(Pangram.REGEX) || []).length === 26;
+    return (
+      (this.text.toLowerCase().match(Pangram.ALPHA_UNIQ_COUNT_PATTERN) || [])
+        .length === 26
+    );
   }
 }
